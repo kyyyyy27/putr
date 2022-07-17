f367ddcbec057f920480e9b97c6e56d52cb6f065
DaniBotz <108689250+YTDANIBOTZ@users.noreply.github.com>
2022-07-17 11:15:45 +0800

    Update dani.js

--- dani.js
+++ dani.js
@@ -537,7 +537,7 @@ Silahkan @${m.mentionedJid[0].split`@`[0]} untuk ketik terima/tolak`
             }
             break
 	    case 'donasi': case 'sewabot': case 'sewa': case 'buypremium': case 'donate': {
-                hisoka.sendMessage(m.chat, { image: { url: 'https://telegra.ph/file/74fd634010128be37972c.jpg' }, caption: `*Hai Kak ${m.pushName}*\n\n Bot Rental Prices\n⭔ 13k Per Group via E-Walet 1 Month\n⭔ 18k via pulsa 1 Month\n\n Premium Price Bot\n⭔ 8k per User 1 bulan\n\nPayment can be via Paypal/link aja/pulsa\n\nFor more details, you can chat with the owner\nhttps://wa.me/6281251549899 (Owner)\n\nDonate For Me : \n\n⭔ Gopay : 081352848936\n⭔ Dana : 081251549899` }, { quoted: m })
+                hisoka.sendMessage(m.chat, { image: { url: 'https://telegra.ph/file/b33b2f04360c1b3c732c9.jpg' }, caption: `*Hai Kak ${m.pushName}*\n\n Bot Rental Prices\n⭔ 13k Per Group via E-Walet 1 Month\n⭔ 18k via pulsa 1 Month\n\n Premium Price Bot\n⭔ 8k per User 1 bulan\n\nPayment can be via Paypal/link aja/pulsa\n\nFor more details, you can chat with the owner\nhttps://wa.me/6281251549899 (Owner)\n\nDonate For Me : \n\n⭔ Gopay : 081352848936\n⭔ Dana : 081251549899` }, { quoted: m })
             }
             break
             case 'sc': {
